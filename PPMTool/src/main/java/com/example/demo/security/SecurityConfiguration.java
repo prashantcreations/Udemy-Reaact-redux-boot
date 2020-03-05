879391644d8eb8838a9b1c9ad20d21478b749058
Prashant Tiwari <prashant.tiwari@yash.com>
2020-03-05 16:39:28 +0530

    sdfv

--- PPMTool/src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ PPMTool/src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -60,6 +60,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     };
 
 
+    
+    
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
--- PPMTool/src/main/java/com/example/demo/security/SwaggerConfig.java
+++ PPMTool/src/main/java/com/example/demo/security/SwaggerConfig.java
@@ -28,6 +28,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * */
 
 
+
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
